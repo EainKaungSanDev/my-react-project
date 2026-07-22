2ef24626a99f7623d71edb3bd93e5b5d17ff8b66
Eain Kaung San <unnoadd2325@gmail.com>
2026-07-22 17:20:31 +0000

    To do List with edit save

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,134 @@
-import {useState,useEffect,ChangeEvent,KeyboardEvent} from "react"
+import {useState,useEffect,ChangeEvent,KeyboardEvent} from 'react'
 
-interface FormProps {
+interface FormList {
     text : string,
     id : number,
     completed : boolean
 }
 
 interface ChildProps {
-    item : FormProps,
+    item : FormList,
     onToggle : (id:number) => void,
-    onDelete : (id:number) => void,
     onEdit : (id:number,text:string) => void,
     onSave : () => void,
-    edit : number | null
+    onDelete : (id:number) => void,
+    editId : number | null,
+    editText : string,
+    setEditText : React.Dispatch<React.SetStateAction<string>>
 }
 
-const Parent = () => {
+const App = () => {
 
 const [input,setInput] = useState<string>("")
 
-const [edit, setEdit] = useState<number | null>(null);
-
-const [array,setArray] = useState<FormProps[]>(()=>{
-    const saved = localStorage.getItem("todos")
+const [array,setArray] = useState<FormList[]>(()=>{
+    const saved  = localStorage.getItem("todos");
     
-    return saved? JSON.parse(saved) as FormProps[] : [] 
-})
+    return saved? JSON.parse(saved) as FormList[] : []
+});
 
-useEffect(() => {
+useEffect(()=>{
     localStorage.setItem("todos",JSON.stringify(array))
 },[array])
+
+const [editId,setEditId] = useState< number | null >(null)
+
+const [editText,setEditText] = useState<string>("")
 
 const Change = (e:ChangeEvent<HTMLInputElement>): void => {
     setInput(e.target.value)
 }
 
 const KeyDown = (e:KeyboardEvent<HTMLInputElement>): void => {
-  if(e.key === "Enter" && input.trim() !== ""){
-  const newList = {
-      text : input,
-      id : Date.now(),
-      completed : false
-  }
-      setArray(prev => [...prev,newList])      
-      setInput("")
-  }
+    if(e.key === "Enter" && input.trim() !== ""){
+        const newList = {
+            text : input,
+            id : Date.now(),
+            completed : false
+        }
+        setArray(prev => [...prev,newList]);
+        setInput("")
+    } 
 }
 
-const Toggle = (id:number): void => {
+const Toggle = (id:number) => {
     setArray(prev => prev.map(item => item.id === id? {...item, completed : !item.completed} : item))
+} 
+
+const Edit = (id:number,text:string) => {
+    setEditId(id);
+    setEditText(text)
 }
 
-const Delete = (id:number): void => {
+const Save = () => {
+if(editId === null) return;
+    setArray(prev => prev.map(item => item.id === editId? {...item, text : editText} : item));
+    setEditId(null);
+    setEditText("");
+}
+
+const Delete = (id:number) => {
     setArray(prev => prev.filter(item => item.id !== id))
 }
 
-const Edit = (id:number,text:string): void => {
-    setEdit(id);
-    setInput(text)
-}
-
-const Save = (): void => {
-if(edit === null) return;
-    setArray(prev => prev.map(item => item.id === edit? {...item, text : input} : item))
-    setEdit(null);
-    setInput("")
-}
-
     return(
+        <>
+        <h1>To Do List</h1>
+        <input type="text" placeholder="To do List" value={input} onChange={Change} onKeyDown={KeyDown} />
+        
+        <ul style={{
+            listStyle : "none"
+        }}>
+            {array.map(item => (
+                <Child key={item.id} 
+item={item}
+onToggle={Toggle}
+onEdit={Edit}
+onSave={Save}
+onDelete={Delete}
+editId={editId}
+editText={editText}
+setEditText={setEditText} />
+            ))}
+        </ul>
+        </>
+    )
+}
+
+const Child = ({item,onToggle,onEdit,onSave,onDelete,editId,editText,setEditText}: ChildProps) => {
+    return(
+        <>
         <div>
-           <h1>To do List</h1>
-
-            <input type="text" value={input} onChange={Change} onKeyDown={KeyDown}/>
-            <div>
-                {array.map(item => (
-<Child key={item.id} 
-item={item} 
-onToggle={Toggle} onDelete={Delete} 
-onEdit={Edit} 
-onSave={Save} 
-edit={edit}/>
-                ))}
-            </div>
+          {
+         editId === item.id?
+          <input value={editText} onChange = { (e:ChangeEvent<HTMLInputElement>) => {
+      setEditText(e.target.value)        
+          }}/> :
+          <span onClick={()=>onToggle(item.id)} style = {{
+              textDecoration : 
+              item.completed? "line-through" : "none",
+              cursor : "pointer"
+          }}>
+              {item.text}
+          </span>
+         }
+         
+         {
+         editId === item.id?
+          <button onClick={()=>onSave()}>
+              Save
+          </button> :
+          <button onClick={()=>onEdit(item.id,item.text)}>
+              Edit
+          </button>
+         }
+         
+         <button onClick={()=>onDelete(item.id)}>
+             Del
+         </button>
         </div>
+        </>
     )
 }
 
-const Child = ({item,onToggle,onDelete,onEdit,onSave,edit} : ChildProps) => {
-    return(
-        <div onClick={()=>onToggle(item.id)} style={{
-            textDecoration : 
-            item.completed? "line-through" : "none",
-            cursor : "pointer"
-        }}>{item.text}
-        {
-         edit === item.id?  
-         <button onClick={(e)=>{
-    e.stopPropagation();
-    onSave()         
-         }}>Save</button> :
-         <button onClick={(e)=>{
-            e.stopPropagation(); onEdit(item.id,item.text)
-         }}>Edit</button>
-        }
-        <button onClick={(e)=>{
-            e.stopPropagation();
-onDelete(item.id)}}>
-Del
-</button>
-        </div>
-    )
-}
-
-export default Parent
+export default App
